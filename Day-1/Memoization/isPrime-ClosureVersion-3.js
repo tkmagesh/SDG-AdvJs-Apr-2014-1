8fed2b87db473aeb349bbefdc87be774da7ed0ed
tkmagesh <tkmagesh77@gmail.com>
2014-04-27 09:50:02 +0530

    Closure Assignment - Solution

---
+++ Day-1/Memoization/isPrime-ClosureVersion-3.js
@@ -0,0 +1,23 @@
+function isPrime(n){
+	var result = {
+		0 : true,
+		1 : true,
+		2 : true,
+		3 : true
+	};
+	isPrime = function(n){
+		if (typeof result[n] !== "undefined"){
+			console.log("returnin from cache...")
+			return result[n];
+		}
+		result[n] = true;
+		for(var i=2;i<=(n/2);i++)
+			if (n%i===0) {
+				result[n] = false;
+				break;
+			}
+		console.log("freshly brewed....");
+		return result[n];
+	};
+	return isPrime(n);
+}
